2ba53d4c49f373282a2e2196fdd5c7e203e41ab2
kine <kine.sandvik07@gmail.com>
2025-08-27 10:20:55 +0200

    created the game, and aded a small tutorial

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ ive made a lot of diffrenet pygames and eazy tutorials on how ot make and play t
 
 
 
+## guessing game !! 
+the purpose of this game is to guess waht number the randomiser has chosen. in the code i also set a limit of guesses, so you cant forever, and it makes the game a bit easier !! so lets go thru how i made it:
+
+in this code im using an import aka a libary, and so i place that at the top of the code:
+
+```python
+import random
+```
+
+secondly you start with a introduction, so the user unserstand whats about to happen: 
+
+```python
+print("this is the number guessing game !!") # and so on ...
+```
+
+
--- guessing.py
+++ guessing.py
@@ -1,2 +1,28 @@
+import random 
 
+print("this is the number guessing game !! u get 10 chances to guess, for each guss it will get easier (i promise).")
+
+low = int(input("now you have to choose in whatt range you want to guess the numbers, so enter the lowest range: "))
+high = int(input("and now the highest number u want: "))
+
+print(f'now you have 10 chances to guss the numbers from {low} to {high}, good luck !!')
+
+num = random.randint(low,high)
+
+chances = 10
+guesses = 0
+
+while guesses < chances:
+    guesses += 1
+    guess = int(input("enter your guess: "))
+
+    if guess == num:
+        print(f'correct! the number is {num}. you guessed it in {guesses} attempts')
+        break 
+    elif guesses >= chances and guess != num:
+        print(f'sorry, the number was {num}. better luck next time !!')
+    elif guess > num: 
+        print("to high !!")
+    elif guess < num: 
+        print("to low !!")
 
